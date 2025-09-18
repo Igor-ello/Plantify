2dfb7a65e4cf52fc61d0d2a9097fd610df041587
Igorello <103700053+Igor-ello@users.noreply.github.com>
2025-09-18 20:17:34 +0300

    Added library lifecycle.viewmodel.compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
 
     // Navigation Compose
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
