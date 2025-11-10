bb2e1119341143dc821878f423dd950fc1aa40b2
Igorello <ioproglab@gmail.com>
2025-11-11 00:48:43 +0300

    Added custom button, checkbox and text

---
+++ app/src/main/java/com/example/myplants/ui/componets/base/AppText.kt
@@ -0,0 +1,143 @@
+package com.example.myplants.ui.componets.base
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myplants.ui.theme.MyPlantsTheme
+
+@Composable
+private fun AppText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onSurface,
+    style: TextStyle = MaterialTheme.typography.bodyMedium,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        style = style,
+        maxLines = maxLines,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun BodyLarge(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onSurface,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    AppText(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.bodyLarge,
+        color = color,
+        maxLines = maxLines,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun BodyMedium(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onSurface,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    AppText(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.bodyMedium,
+        color = color,
+        maxLines = maxLines,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun TitleLarge(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onBackground,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    AppText(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.titleLarge,
+        color = color,
+        maxLines = maxLines,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun LabelLarge(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onSurface,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign = TextAlign.Center
+) {
+    AppText(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelLarge,
+        color = color,
+        maxLines = maxLines,
+        textAlign = textAlign
+    )
+}
+
+@Composable
+fun LabelSmall(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.onSurface,
+    maxLines: Int = Int.MAX_VALUE,
+    textAlign: TextAlign = TextAlign.Start
+) {
+    AppText(
+        text = text,
+        modifier = modifier,
+        style = MaterialTheme.typography.labelSmall,
+        color = color,
+        maxLines = maxLines,
+        textAlign = textAlign
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AppTextAllPreview() {
+    MyPlantsTheme {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            TitleLarge("TitleLarge: Крупный заголовок")
+            BodyLarge("BodyLarge: Основной текст приложения")
+            BodyMedium("BodyMedium: Второстепенный текст, подписи, пояснения")
+            LabelLarge("LabelLarge: Текст на кнопках, ярлыках, метках")
+            LabelSmall("LabelSmall: Мелкий текст для подсказок, маленьких подписей")
+        }
+    }
+}
