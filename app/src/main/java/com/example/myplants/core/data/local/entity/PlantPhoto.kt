a2ec251cb07e8288798606d641cb43fc40e629c8
Igorello <103700053+Igor-ello@users.noreply.github.com>
2025-12-06 01:18:15 +0300

    refactor: Added new filed imageData in PlantPhoto

--- app/src/main/java/com/example/myplants/core/data/local/entity/PlantPhoto.kt
+++ app/src/main/java/com/example/myplants/core/data/local/entity/PlantPhoto.kt
@@ -28,11 +28,40 @@ data class PlantPhoto(
     val plantId: Long,
 
     @ColumnInfo(name = "uri")
-    val uri: String,
+    val uri: String? = null,
+
+    @ColumnInfo(name = "image_data", typeAffinity = ColumnInfo.BLOB)
+    val imageData: ByteArray? = null,
 
     @ColumnInfo(name = "is_primary")
     val isPrimary: Boolean = false,
 
     @ColumnInfo(name = "created_at")
     val createdAt: Long = System.currentTimeMillis()
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PlantPhoto
+
+        if (id != other.id) return false
+        if (plantId != other.plantId) return false
+        if (isPrimary != other.isPrimary) return false
+        if (createdAt != other.createdAt) return false
+        if (uri != other.uri) return false
+        if (!imageData.contentEquals(other.imageData)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + plantId.hashCode()
+        result = 31 * result + isPrimary.hashCode()
+        result = 31 * result + createdAt.hashCode()
+        result = 31 * result + uri.hashCode()
+        result = 31 * result + (imageData?.contentHashCode() ?: 0)
+        return result
+    }
+}
